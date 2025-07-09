a6eb00a2c64e1403b79dcc7daa2f257a6c95aea5
Oleg Vishnivetsky <vorboleg@gmail.com>
2025-07-09 19:36:28 +0300

    DefineSymbolInstaller fix

--- NodeBasedDialog/Assets/Plugins/DialogNodeBasedSystem/Scripts/Editor/DefineSymbolInstaller.cs
+++ NodeBasedDialog/Assets/Plugins/DialogNodeBasedSystem/Scripts/Editor/DefineSymbolInstaller.cs
@@ -6,76 +6,79 @@ using UnityEditor.PackageManager;
 using UnityEditor.PackageManager.Requests;
 using UnityEngine;
 
-[InitializeOnLoad]
-public static class DefineSymbolInstaller
+namespace cherrydev
 {
-    private const string TimelinePackageName = "com.unity.timeline";
-    private const string TimelineDefineSymbol = "UNITY_TIMELINE";
-
-    private const string LocalizationPackageName = "com.unity.localization";
-    private const string LocalizationDefineSymbol = "UNITY_LOCALIZATION";
-
-    private static ListRequest _listRequest;
-    private static string EditorPrefKey => $"DefineSymbolInstaller_HasChecked_{Application.dataPath.GetHashCode()}";
-
-    static DefineSymbolInstaller()
+    [InitializeOnLoad]
+    public static class DefineSymbolInstaller
     {
-        EditorApplication.delayCall += () =>
+        private const string TimelinePackageName = "com.unity.timeline";
+        private const string TimelineDefineSymbol = "UNITY_TIMELINE";
+
+        private const string LocalizationPackageName = "com.unity.localization";
+        private const string LocalizationDefineSymbol = "UNITY_LOCALIZATION";
+
+        private static ListRequest _listRequest;
+        private static string EditorPrefKey => $"DefineSymbolInstaller_HasChecked_{Application.dataPath.GetHashCode()}";
+
+        static DefineSymbolInstaller()
         {
-            if (!EditorPrefs.GetBool(EditorPrefKey, false))
+            EditorApplication.delayCall += () =>
             {
-                EditorPrefs.SetBool(EditorPrefKey, true);
-                CheckPackagesAndDefineSymbols();
+                if (!EditorPrefs.GetBool(EditorPrefKey, false))
+                {
+                    EditorPrefs.SetBool(EditorPrefKey, true);
+                    CheckPackagesAndDefineSymbols();
+                }
+            };
+        }
+
+        [MenuItem("Tools/Dialog System/Check Define Symbols")]
+        public static void ForceCheckDefineSymbols() => CheckPackagesAndDefineSymbols();
+
+        public static void CheckPackagesAndDefineSymbols()
+        {
+            Debug.Log("Check");
+            _listRequest = Client.List(true, false);
+            EditorApplication.update += OnListProgress;
+        }
+
+        private static void OnListProgress()
+        {
+            if (!_listRequest.IsCompleted)
+                return;
+
+            if (_listRequest.Status == StatusCode.Success)
+            {
+                bool timelineInstalled = _listRequest.Result.Any(pkg => pkg.name == TimelinePackageName);
+                bool localizationInstalled = _listRequest.Result.Any(pkg => pkg.name == LocalizationPackageName);
+
+                UpdateDefineSymbol(TimelineDefineSymbol, timelineInstalled);
+                UpdateDefineSymbol(LocalizationDefineSymbol, localizationInstalled);
             }
-        };
-    }
-
-    [MenuItem("Tools/Dialog System/Check Define Symbols")]
-    public static void ForceCheckDefineSymbols() => CheckPackagesAndDefineSymbols();
-
-    public static void CheckPackagesAndDefineSymbols()
-    {
-        Debug.Log("Check");
-        _listRequest = Client.List(true, false);
-        EditorApplication.update += OnListProgress;
-    }
-
-    private static void OnListProgress()
-    {
-        if (!_listRequest.IsCompleted)
-            return;
-
-        if (_listRequest.Status == StatusCode.Success)
-        {
-            bool timelineInstalled = _listRequest.Result.Any(pkg => pkg.name == TimelinePackageName);
-            bool localizationInstalled = _listRequest.Result.Any(pkg => pkg.name == LocalizationPackageName);
-
-            UpdateDefineSymbol(TimelineDefineSymbol, timelineInstalled);
-            UpdateDefineSymbol(LocalizationDefineSymbol, localizationInstalled);
-        }
         
-        EditorApplication.update -= OnListProgress;
-    }
-
-    private static void UpdateDefineSymbol(string defineSymbol, bool shouldEnable)
-    {
-        NamedBuildTarget namedBuildTarget = NamedBuildTarget.FromBuildTargetGroup(EditorUserBuildSettings.selectedBuildTargetGroup);
-        PlayerSettings.GetScriptingDefineSymbols(namedBuildTarget, out string[] currentDefines);
-        List<string> definesList = currentDefines.ToList();
-        
-        bool contains = definesList.Contains(defineSymbol);
-
-        if (shouldEnable && !contains)
-        {
-            definesList.Add(defineSymbol);
-            PlayerSettings.SetScriptingDefineSymbols(namedBuildTarget, definesList.ToArray());
-            Debug.Log($"[DefineSymbolManager] Added {defineSymbol} define symbol for {namedBuildTarget.TargetName}");
+            EditorApplication.update -= OnListProgress;
         }
-        else if (!shouldEnable && contains)
+
+        private static void UpdateDefineSymbol(string defineSymbol, bool shouldEnable)
         {
-            definesList.Remove(defineSymbol);
-            PlayerSettings.SetScriptingDefineSymbols(namedBuildTarget, definesList.ToArray());
-            Debug.Log($"[DefineSymbolManager] Removed {defineSymbol} define symbol for {namedBuildTarget.TargetName}");
+            NamedBuildTarget namedBuildTarget = NamedBuildTarget.FromBuildTargetGroup(EditorUserBuildSettings.selectedBuildTargetGroup);
+            PlayerSettings.GetScriptingDefineSymbols(namedBuildTarget, out string[] currentDefines);
+            List<string> definesList = currentDefines.ToList();
+        
+            bool contains = definesList.Contains(defineSymbol);
+
+            if (shouldEnable && !contains)
+            {
+                definesList.Add(defineSymbol);
+                PlayerSettings.SetScriptingDefineSymbols(namedBuildTarget, definesList.ToArray());
+                Debug.Log($"[DefineSymbolManager] Added {defineSymbol} define symbol for {namedBuildTarget.TargetName}");
+            }
+            else if (!shouldEnable && contains)
+            {
+                definesList.Remove(defineSymbol);
+                PlayerSettings.SetScriptingDefineSymbols(namedBuildTarget, definesList.ToArray());
+                Debug.Log($"[DefineSymbolManager] Removed {defineSymbol} define symbol for {namedBuildTarget.TargetName}");
+            }
         }
     }
 }
